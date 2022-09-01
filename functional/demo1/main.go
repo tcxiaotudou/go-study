c1399c8ef093640f5ce6cbb9bbeba97dcdd9475e
fciasth小土豆 <2290262044@qq.com>
2022-09-01 18:18:10 +0800

    functional

---
+++ functional/demo1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Decorator(decoPtr, fn interface{}) (err error) {
+	var decoratedFunc, targetFunc reflect.Value
+	decoratedFunc = reflect.ValueOf(decoPtr).Elem()
+	targetFunc = reflect.ValueOf(fn)
+	v := reflect.MakeFunc(targetFunc.Type(),
+		func(in []reflect.Value) (out []reflect.Value) {
+			fmt.Println("before")
+			out = targetFunc.Call(in)
+			fmt.Println("after")
+			return
+		})
+	decoratedFunc.Set(v)
+	return
+}
+
+func decorator(f func(s string)) func(s string) {
+	return func(s string) {
+		fmt.Println("Started")
+		f(s)
+		fmt.Println("Done")
+	}
+}
+
+func Hello(s string) {
+	fmt.Println(s)
+}
+
+func main() {
+	decorator(Hello)("Hello, World!")
+}
