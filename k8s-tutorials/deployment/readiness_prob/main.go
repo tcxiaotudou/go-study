2a95c479550deaf8e9aaadc237030a9a0b26e4ec
fciasth小土豆 <2290262044@qq.com>
2022-12-25 21:07:39 +0800

    k8s

---
+++ k8s-tutorials/deployment/readiness_prob/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "[v1] Hello, Kubernetes!")
+}
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(500)
+	})
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
